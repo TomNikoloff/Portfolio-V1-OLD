4d1b279a387123be8cfd10d2d00ea2f37c2606fd
TomNikoloff <tomnikoloff@hotmail.co.uk>
2021-07-03 12:22:00 +0100

    Added more blackjack JS

--- js/blackjack.js
+++ js/blackjack.js
@@ -6,16 +6,41 @@ let blackjackGame = {
 }
 
 document.querySelector('#blackjack-hit-button').addEventListener('click', blackjackHit);
-document.querySelector('#blackjack-deal-button').addEventListener('click', blackjackDeal);
 
 const YOU = blackjackGame['you'];
 const DEALER = blackjackGame['dealer'];
 
+const hitSound = new Audio('sounds/swish.m4a');
+
 function blackjackHit() {
     let card = randomCard();
+    showCard(card, YOU);
 }
 
 function randomCard() {
     let randomIndex = Math.floor(Math.random() * 13);
     return blackjackGame['cards'][randomIndex];
+}
+
+function showCard(card, activePlayer) {
+    let cardImage = document.createElement('img');
+    cardImage.src = `images/${card}.png`;
+    document.querySelector(activePlayer['div']).appendChild(cardImage);
+    hitSound.play();
+}
+
+document.querySelector('#blackjack-deal-button').addEventListener('click', blackjackDeal);
+
+function blackjackDeal() {
+    let yourImages = document.querySelector('#your-box').querySelectorAll('img');
+
+    for (let i = 0; i < yourImages.length; i++) {
+        yourImages[i].remove();
+    }
+
+    let dealerImages = document.querySelector('#dealer-box').querySelectorAll('img');
+
+    for (let i = 0; i < dealerImages.length; i++) {
+        dealerImages[i].remove();
+    }
 }
